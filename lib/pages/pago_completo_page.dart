eee4eddb2918c082ca384dfa5758e22a34be1f81
Orlin Alvarado <oalvaradomorales@gmail.com>
2020-11-12 07:33:32 -0600

    Diseño finalizado

---
+++ lib/pages/pago_completo_page.dart
@@ -0,0 +1,26 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class PagoCompletoPage extends StatelessWidget {
+
+@override
+  Widget build(BuildContext context) {
+   return Scaffold(
+      appBar: AppBar(
+           title: Text('Pago realizado!', textAlign: TextAlign.center,)
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon( FontAwesomeIcons.star, color: Colors.white54, size: 100),
+            SizedBox( height: 20),
+            Text('Pago realizado correctamente', style: TextStyle( color: Colors.white, fontSize: 22),)
+          ],
+        )
+      ),
+    );
+  }
+}
